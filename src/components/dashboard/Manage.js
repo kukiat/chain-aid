4d60a42af26b869c17571a8228aa81383b7f01d4
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-08-11 17:50:02 +0700

    🎨 Set layout

--- src/components/dashboard/Manage.js
+++ src/components/dashboard/Manage.js
@@ -7,18 +7,20 @@ import classNames from 'classnames';
 class Manage extends React.Component {
   render() {
     return (
-      <div className="Manage">
+      <div className="manage col-md-10">
         <div className="row">
-          <div className="col-md-8 col-sm" style={{ backgroundColor: 'red', height: '1000px' }}>
-          
-          </div>
-          <div className="col-md-4 col-sm" style={{backgroundColor: 'blue'}}>
-          
-          </div>
+          <div
+            className="col-md-8 col-sm"
+            style={{ backgroundColor: 'red', height: '1000px' }}
+          />
+          <div
+            className="col-md-4 col-sm"
+            style={{ backgroundColor: 'blue' }}
+          />
         </div>
       </div>
     );
   }
 }
 
-export default Layout(Manage)
+export default Layout(Manage);
