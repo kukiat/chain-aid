398819fd730d0fd16cc5a7e01007f22a2d5c201d
kukiat <kukiat1996@gmail.com>
2018-08-11 21:48:53 +0700

    fixed nav padding

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import logo from './logo.jpg'
 import '../../Styles/Nav.css'
+import { Link } from 'react-router-dom'
 
 export default class Nav extends React.Component {
   render() {
     return(
       <nav className="navbar">
         <div className="nav-container">
-          <img className="nav-item imgg" src = {logo} width="34" height="34"/>
-          <h4 className="nav-item">Chain-AID: The Electric Medical Record on Blockchain</h4>
+          <Link to="/">
+            <img className="nav-item imgg" src = {logo} width="34" height="34"/>
+          </Link>
+          <div className="navavav">
+            <div className="nav-item">Chain-AID: The Electric Medical Record on Blockchain</div>
+          </div>
         </div>
       </nav>
     )
