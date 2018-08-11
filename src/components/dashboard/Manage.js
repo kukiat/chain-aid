98f987b7aa3bd47e9a06306ebe7873a566395cce
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-08-11 17:35:26 +0700

    🎨 Set layout for Manage

--- src/components/dashboard/Manage.js
+++ src/components/dashboard/Manage.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Layout } from '../Layout'
-import Nav from '../navbar/Nav'
-import Navigation from '../navigation/Navigation'
+import React from 'react';
+import { Layout } from '../Layout';
+import Nav from '../navbar/Nav';
+import Navigation from '../navigation/Navigation';
+import classNames from 'classnames';
 
 class Manage extends React.Component {
   render() {
-    return(
-      <div>
-        <Nav/>
-        <Navigation/>
-        Manage
+    return (
+      <div className="Manage">
+        <Nav />
+        <Navigation />
+        <div className="row">
+          <div className="col-md-8 col-sm" style={{ backgroundColor: 'red', height: '1000px' }}>
+          
+          </div>
+          <div className="col-md-4 col-sm" style={{backgroundColor: 'blue'}}>
+          
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
 // export default Layout(Manage)
-export default Manage
+export default Manage;
