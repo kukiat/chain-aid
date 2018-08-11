c18d7951f5b9253e1c8e46b20a62109b81c467c5
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-08-11 18:58:59 +0700

    🎨 Kao update naja

--- src/components/dashboard/Manage.js
+++ src/components/dashboard/Manage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Layout } from '../Layout';
 import Nav from '../navbar/Nav';
 import Navigation from '../navigation/Navigation';
-import classNames from 'classnames';
 import '../../Styles/Manage.css';
 
 class Manage extends React.Component {
@@ -17,14 +16,16 @@ class Manage extends React.Component {
       <div className="manage col-md-10">
         <div className="row">
           <div
-            className="col-md-9 col-sm form"
+            className="col-md-11 col-sm form"
             style={{height: '1000px' }}
           >
-            <h3 className="inline">จัดการข้อมูลผู้ป่วย</h3>
-            <span style={{ marginLeft: '15px' }}>
-              แก้ไขครั้งล่าสุดเมื่อ {this.state.lastModifile}
-            </span>
-            <div style={{ width: '100%', padding: '10px 30px' }}>
+            <div>
+              <h3 className="inline">จัดการข้อมูลผู้ป่วย</h3>
+              <span style={{ marginLeft: '15px' }}>
+                แก้ไขครั้งล่าสุดเมื่อ {this.state.lastModifile}
+              </span>
+            </div>
+            <div style={{ padding: '10px 30px' }} className="inline">
               <div style={{ marginTop: '10px' }}>
                 <h5 className="inline">หมายเลขประจำตัวผู้ป่วย</h5>
                 <input type="text" className="input-200" />
@@ -38,42 +39,69 @@ class Manage extends React.Component {
                 <input type="text" className="input-200" />
               </div>
             </div>
+            <div className="inline" style={{marginLeft: '20vw'}}>
+              <div style={{ height: '100px', width: '100px', backgroundColor: 'red', borderRadius: '50%' }}></div>
+            </div>
             <div style={{ width: '100%', padding: '20px' }}>
               <h4>ข้อมูลส่วนตัว</h4>
               <hr />
               <div style={{ width: '100%', padding: '10px 30px' }}>
-                <div>
-                  <h5 className="inline">ชื่อ</h5>
-                  <input type="text" className="input-150" />
-                  <h5 className="inline" style={{marginLeft: '30px' }}>นามสกุล</h5>
-                  <input type="text" className="input-150" />
-                  <h5 className="inline" style={{ marginLeft: '30px' }}>ชื่อเล่น</h5>
-                  <input type="text" className="input-100" />
+                <div className="row">
+                  <div className="col">
+                    <h5 className="inline">ชื่อ</h5>
+                    <input type="text" className="input-150" />
+                  </div>
+                  <div className="col">
+                    <h5 className="inline" style={{marginLeft: '30px' }}>นามสกุล</h5>
+                    <input type="text" className="input-150" />
+                  </div>
+                  <div className="col">
+                    <h5 className="inline" style={{ marginLeft: '30px' }}>ชื่อเล่น</h5>
+                    <input type="text" className="input-100" />
+                  </div>
                 </div>
-                <div style={{marginTop: '20px'}}>
-                  <h5 className="inline">เพศ</h5>
-                  <input type="text" className="input-150" />
-                  <h5 className="inline" style={{ marginLeft: '30px' }}>วันเกิด</h5>
-                  <input type="text" className="input-150" />
-                  <h5 className="inline" style={{ marginLeft: '30px' }}>อายุ</h5>
-                  <input type="text" className="input-50"/>
+                <div style={{marginTop: '20px'}} className="row">
+                  <div className="col">
+                    <h5 className="inline">เพศ</h5>
+                    <input type="text" className="input-150" />
+                  </div>
+                  <div className="col">
+                    <h5 className="inline" style={{ marginLeft: '30px' }}>วันเกิด</h5>
+                    <input type="text" className="input-150" />
+                  </div>
+                  <div className="col">
+                    <h5 className="inline" style={{ marginLeft: '30px' }}>อายุ</h5>
+                    <input type="text" className="input-50"/>
+                  </div>
                 </div>
-                <div style={{ marginTop: '20px' }}>
+                <div style={{ marginTop: '20px' }} className="row">
+                <div className="col">
                   <h5 className="inline">ส่วนสูง</h5>
                   <input type="text" className="input-50" />
+                </div>
+                <div className="col">
                   <h5 className="inline" style={{ marginLeft: '30px' }}>น้ำหนัก</h5>
                   <input type="text" className="input-50" />
+                </div>
+                <div className="col">
                   <h5 className="inline" style={{ marginLeft: '30px' }}>หมู่เลือด</h5>
                   <input type="text" className="input-50" />
                 </div>
+                </div>
               </div>
-              <div style={{ marginTop: '20px' }}>
+              <div style={{ marginTop: '20px' }} className="row">
+              <div className="col">
                 <h5 className="inline" style={{ marginLeft: '30px' }}>สัญชาติ</h5>
                 <input type="text" className="input-100" />
+              </div>
+              <div className="col">
                 <h5 className="inline" style={{ marginLeft: '30px' }}>เชื้อชาติ</h5>
                 <input type="text" className="input-100" />
+              </div>
+              <div className="col">
                 <h5 className="inline" style={{ marginLeft: '30px' }}>ศาสนา</h5>
                 <input type="text" className="input-100" />
+              </div>
               </div>
               <div style={{ marginTop: '20px' }}>
                 <h5 className="inline" style={{ marginLeft: '30px' }}>โรคประจำตัว</h5>
@@ -102,9 +130,6 @@ class Manage extends React.Component {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-3 col-sm" style={{ backgroundColor: 'blue' }}>
-            รูป
           </div>
         </div>
       </div>
