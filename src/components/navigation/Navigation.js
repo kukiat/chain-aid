8c1966a58001229e76873d6fd56f3e80ca5b419a
blaster666 <aiy.spell@gmail.com>
2018-08-15 23:49:09 +0700

    margin sas2

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -7,32 +7,32 @@ export default class Navigation extends React.Component {
     return (
       <div className="navigation col-md-2">
         <NavLink activeClassName="wrap-btn-active" className="wrap-btn" to="/manage">
-          <div className="navigation-btn">
+          <div className="navigation-btn left">
             <p>จัดการข้อมูลผู้ป่วย</p>
           </div>
         </NavLink>
         <Link className="wrap-btn" to="/">
-          <div className="navigation-btn">
+          <div className="navigation-btn left">
             <p>สิทธิการรักษา</p>
           </div>
         </Link>
         <Link className="wrap-btn" to="/">
-          <div className="navigation-btn">
+          <div className="navigation-btn left">
             <p>ประวัติการรักษา</p>
           </div>
         </Link>
         <Link className="wrap-btn" to="/">
-          <div className="navigation-btn">
+          <div className="navigation-btn left">
             <p>ประวัติการจ่ายยา</p>
           </div>
         </Link>
         <Link className="wrap-btn" to="/">
-          <div className="navigation-btn">
+          <div className="navigation-btn left">
             <p>ครอบครัว</p>
           </div>
         </Link>
         <Link className="wrap-btn" to="/">
-          <div className="navigation-btn">
+          <div className="navigation-btn left">
             <p>จัดการข้อมูลผู้ป่วย</p>
           </div>
         </Link>
