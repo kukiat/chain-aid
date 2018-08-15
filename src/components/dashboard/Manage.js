74b89c72dd55c7bcba3e55775ca0c4be00d9953e
kukiat <kukiat1996@gmail.com>
2018-08-16 03:05:55 +0700

    fix handle form

--- src/components/dashboard/Manage.js
+++ src/components/dashboard/Manage.js
@@ -7,150 +7,296 @@ class Manage extends React.Component {
     super();
     this.state = {
       lastModifile: '2018-11-8',
-      data: []
+      data: [],
+      isLoading: true
     };
   }
+
   componentDidMount(){
-    fetch('https://chain-aid-api.herokuapp.com/api/manage/?cardid=5704062616518').
-    then(response => response.json()).
-    then(result => {
-      this.setState({ data: result.results[0]})})
+    fetch('https://chain-aid-api.herokuapp.com/api/manage/?cardId=5704062616518')
+      .then(response => response.json())
+      .then(({ results }) => {
+        this.setState({ 
+          data: results,
+          isLoading: false
+        })
+      })
   }
-  show(value, value2){
-    if (this.state.data[value2] != undefined){
-      return  this.state.data[value2][value]
-    }
-    return ""
+
+  handleManageId = (e) => {
+    const { data } = this.state
+    this.setState({
+      data: Object.assign(data, { manageId: Object.assign(data.manageId, { [e.target.name]: e.target.value })})
+    })
   }
-  age(birthday){
-      let textArr = birthday.split('-')
-      let dob = new Date(textArr[2]-543, textArr[1], textArr[0])
-      let diff_ms = Date.now() - dob.getTime();
-      let age_dt = new Date(diff_ms); 
-      return Math.abs(age_dt.getUTCFullYear() - 1970);
+
+  handlePersonalInfo = (e) => {
+    const { data } = this.state
+    this.setState({
+      data: Object.assign(data, { personalInfo: Object.assign(data.personalInfo, { [e.target.name]: e.target.value })})
+    })
   }
+
+  handlePatientInformation = (e) => {
+    const { data } = this.state
+    this.setState({
+      data: Object.assign(data, { patientInformation: Object.assign(data.patientInformation, { [e.target.name]: e.target.value })})
+    })
+  }
+  
   render() {
+    const { data, isLoading } = this.state
+    console.log(data)
     return (
-      <div className="mgl-auto col-md-10">
-        <div className="row mg0">
-          <div
-            className="col-md-11 col-sm form"
-            style={{height: '1000px' }}
-          >
-            <div>
-              <h3 className="inline">จัดการข้อมูลผู้ป่วย</h3>
-              <span style={{ marginLeft: '15px' }}>
-                แก้ไขครั้งล่าสุดเมื่อ {this.state.lastModifile}
-              </span>
-            </div>
-            <div style={{ padding: '10px 30px' }} className="inline">
-              <div style={{ marginTop: '10px' }}>
-                <h5 className="inline">หมายเลขประจำตัวผู้ป่วย</h5>
-                <input type="text" className="input-200" value={this.show("patientId","manageId")}/>
+      isLoading ? <h4>Loading...</h4> :
+        <div className="mgl-auto col-md-10">
+          <div className="row mg0">
+            <div
+              className="col-md-11 col-sm form"
+              style={{height: '1000px' }}
+            >
+              <div>
+                <h3 className="inline">จัดการข้อมูลผู้ป่วย</h3>
+                <span style={{ marginLeft: '15px' }}>
+                  แก้ไขครั้งล่าสุดเมื่อ {this.state.lastModifile}
+                </span>
               </div>
-              <div style={{ marginTop: '10px' }}>
-                <h5 className="inline">หมายเลขประจำตัวประชาชน</h5>
-                <input type="text" className="input-200" value={this.show("cardId","manageId")}/>
-              </div>
-              <div style={{ marginTop: '10px' }}>
-                <h5 className="inline">หมายเลขประจำตัวต่างด้าว</h5>
-                <input type="text" className="input-200" value={this.show("alienId","manageId")}/>
-              </div>
-            </div>
-            <div className="inline" style={{marginLeft: '20vw'}}>
-              <div style={{ height: '120px', width: '120px', backgroundColor: 'red', borderRadius: '50%' }}></div>
-            </div>
-            <div style={{ width: '100%', padding: '20px' }}>
-              <h4>ข้อมูลส่วนตัว</h4>
-              <hr />
-              <div style={{ width: '100%', padding: '10px 30px' }}>
-                <div className="row">
-                  <div className="col">
-                    <h5 className="inline">ชื่อ</h5>
-                    <input type="text" className="input-150" value={this.show("name","personalInfo")}/>
-                  </div>
-                  <div className="col">
-                    <h5 className="inline" style={{marginLeft: '30px' }}>นามสกุล</h5>
-                    <input type="text" className="input-150" value={this.show("surname","personalInfo")}/>
-                  </div>
-                  <div className="col">
-                    <h5 className="inline" style={{ marginLeft: '30px' }}>ชื่อเล่น</h5>
-                    <input type="text" className="input-100" value={this.show("nickname","personalInfo")}/>
-                  </div>
+              <div style={{ padding: '10px 30px' }} className="inline">
+                <div style={{ marginTop: '10px' }}>
+                  <h5 className="inline">หมายเลขประจำตัวผู้ป่วย</h5>
+                  <input 
+                    type="text" 
+                    onChange={this.handleManageId} 
+                    className="input-200" 
+                    value={data.manageId.patientId}
+                    name="patientId"
+                  />
                 </div>
-                <div style={{marginTop: '20px'}} className="row">
+                <div style={{ marginTop: '10px' }}>
+                  <h5 className="inline">หมายเลขประจำตัวประชาชน</h5>
+                  <input 
+                    type="text" 
+                    onChange={this.handleManageId} 
+                    className="input-200" 
+                    value={data.manageId.cardId}
+                    name="cardId"
+                  />
+                </div>
+                <div style={{ marginTop: '10px' }}>
+                  <h5 className="inline">หมายเลขประจำตัวต่างด้าว</h5>
+                  <input 
+                    type="text" 
+                    onChange={this.handleManageId} 
+                    className="input-200" 
+                    value={data.manageId.alienId}
+                    name="alienId"
+                  />
+                </div>
+              </div>
+              <div className="inline" style={{marginLeft: '20vw'}}>
+                <div style={{ height: '120px', width: '120px', backgroundColor: 'red', borderRadius: '50%' }}></div>
+              </div>
+              <div style={{ width: '100%', padding: '20px' }}>
+                <h4>ข้อมูลส่วนตัว</h4>
+                <hr />
+                <div style={{ width: '100%', padding: '10px 30px' }}>
+                  <div className="row">
+                    <div className="col">
+                      <h5 className="inline">ชื่อ</h5>
+                      <input 
+                        type="text" 
+                        className="input-150" 
+                        value={data.personalInfo.name}
+                        onChange={this.handlePersonalInfo}
+                        name="name"
+                      />
+                    </div>
+                    <div className="col">
+                      <h5 className="inline" style={{marginLeft: '30px' }}>นามสกุล</h5>
+                      <input 
+                        type="text" 
+                        className="input-150" 
+                        value={data.personalInfo.surname}
+                        onChange={this.handlePersonalInfo}
+                        name="surname"
+                      />
+                    </div>
+                    <div className="col">
+                      <h5 className="inline" style={{ marginLeft: '30px' }}>ชื่อเล่น</h5>
+                      <input 
+                        type="text" 
+                        className="input-100" 
+                        value={data.personalInfo.nickname}
+                        onChange={this.handlePersonalInfo}
+                        name="nickname"
+                      />
+                    </div>
+                  </div>
+                  <div style={{marginTop: '20px'}} className="row">
+                    <div className="col">
+                      <h5 className="inline">เพศ</h5>
+                      <input 
+                        type="text" 
+                        className="input-150" 
+                        value={data.personalInfo.gender}
+                        onChange={this.handlePersonalInfo}
+                        name="gender"
+                      />
+                    </div>
+                    <div className="col">
+                      <h5 className="inline" style={{ marginLeft: '30px' }}>วันเกิด</h5>
+                      <input 
+                        type="text" 
+                        className="input-150" 
+                        value={data.personalInfo.birthday}
+                        onChange={this.handlePersonalInfo}
+                        name="birthday"
+                      />
+                    </div>
+                    <div className="col">
+                      <h5 className="inline" style={{ marginLeft: '30px' }}>อายุ</h5>
+                      <input 
+                        type="text" 
+                        className="input-50" 
+                        // value={data.personalInfo.age}
+                        // onChange={this.handlePersonalInfo}
+                        name="age"
+                      />
+                    </div>
+                  </div>
+                  <div style={{ marginTop: '20px' }} className="row">
                   <div className="col">
-                    <h5 className="inline">เพศ</h5>
-                    <input type="text" className="input-150" value={this.show("gender","personalInfo")}/>
+                    <h5 className="inline">ส่วนสูง</h5>
+                    <input 
+                      type="text" 
+                      className="input-50" 
+                      value={data.personalInfo.height}
+                      onChange={this.handlePersonalInfo}
+                      name="height"
+                    />
                   </div>
                   <div className="col">
-                    <h5 className="inline" style={{ marginLeft: '30px' }}>วันเกิด</h5>
-                    <input type="text" className="input-150" value={this.show("birthday","personalInfo")}/>
+                    <h5 className="inline" style={{ marginLeft: '30px' }}>น้ำหนัก</h5>
+                    <input 
+                      type="text" 
+                      className="input-50" 
+                      value={data.personalInfo.weight}
+                      onChange={this.handlePersonalInfo}
+                      name="weight"
+                    />
                   </div>
                   <div className="col">
-                    <h5 className="inline" style={{ marginLeft: '30px' }}>อายุ</h5>
-                    <input type="text" className="input-50" value={this.age(this.show("birthday","personalInfo"))}/>
+                    <h5 className="inline" style={{ marginLeft: '30px' }}>หมู่เลือด</h5>
+                    <input 
+                      type="text" 
+                      className="input-50" 
+                      value={data.personalInfo.bloodType}
+                      onChange={this.handlePersonalInfo}
+                      name="bloodType"
+                    />
+                  </div>
                   </div>
                 </div>
                 <div style={{ marginTop: '20px' }} className="row">
                 <div className="col">
-                  <h5 className="inline">ส่วนสูง</h5>
-                  <input type="text" className="input-50" value={this.show("height","personalInfo")}/>
+                  <h5 className="inline" style={{ marginLeft: '30px' }}>สัญชาติ</h5>
+                  <input 
+                    type="text" 
+                    className="input-100" 
+                    value={data.personalInfo.nation}
+                    onChange={this.handlePersonalInfo}
+                    name="nation"
+                  />
                 </div>
                 <div className="col">
-                  <h5 className="inline" style={{ marginLeft: '30px' }}>น้ำหนัก</h5>
-                  <input type="text" className="input-50" value={this.show("weight","personalInfo")}/>
+                  <h5 className="inline" style={{ marginLeft: '30px' }}>เชื้อชาติ</h5>
+                  <input 
+                    type="text" 
+                    className="input-100" 
+                    value={data.personalInfo.race}
+                    onChange={this.handlePersonalInfo}
+                    name="race"
+                  />
                 </div>
                 <div className="col">
-                  <h5 className="inline" style={{ marginLeft: '30px' }}>หมู่เลือด</h5>
-                  <input type="text" className="input-50" value={this.show("bloodType","personalInfo")}/>
+                  <h5 className="inline" style={{ marginLeft: '30px' }}>ศาสนา</h5>
+                  <input 
+                    type="text" 
+                    className="input-100" 
+                    value={data.personalInfo.religion}
+                    onChange={this.handlePersonalInfo}
+                    name="religion"
+                  />
                 </div>
                 </div>
-              </div>
-              <div style={{ marginTop: '20px' }} className="row">
-              <div className="col">
-                <h5 className="inline" style={{ marginLeft: '30px' }}>สัญชาติ</h5>
-                <input type="text" className="input-100" value={this.show("nation","personalInfo")}/>
-              </div>
-              <div className="col">
-                <h5 className="inline" style={{ marginLeft: '30px' }}>เชื้อชาติ</h5>
-                <input type="text" className="input-100" value={this.show("race","personalInfo")}/>
-              </div>
-              <div className="col">
-                <h5 className="inline" style={{ marginLeft: '30px' }}>ศาสนา</h5>
-                <input type="text" className="input-100" value={this.show("religion","personalInfo")}/>
-              </div>
-              </div>
-              <div style={{ marginTop: '20px' }}>
-                <h5 className="inline" style={{ marginLeft: '30px' }}>โรคประจำตัว</h5>
-                <input type="text" className="input-200" value={this.show("congenital","personalInfo")}/>
-                <h5 className="inline" style={{ marginLeft: '30px' }}>ยาที่แพ้</h5>
-                <input type="text" className="input-200" value={this.show("allergyDrug","personalInfo")}/>
-              </div>
-              <div style={{ marginTop: '20px' }}>
-                <h5 className="inline" style={{ marginLeft: '30px' }}>ผู้ติดต่อฉุกเฉิน</h5>
-                <input type="text" className="input-200" value={this.show("contact","personalInfo")}/>
-              </div>
-            </div>
-            <div style={{ width: '100%', padding: '20px' }}>
-              <h4>ข้อมูลติดต่อของผู้ป่วย</h4>
-              <hr />
-              <div style={{ marginLeft: '30px' }}>
-                <div>
-                  <h5>ที่อยู่ปัจจุบัน</h5>
-                  <textarea cols="80" rows="4">{this.show("address","patientInformation")}</textarea>
+                <div style={{ marginTop: '20px' }}>
+                  <h5 className="inline" style={{ marginLeft: '30px' }}>โรคประจำตัว</h5>
+                  <input 
+                    type="text" 
+                    className="input-200" 
+                    value={data.personalInfo.congenital}
+                    onChange={this.handlePersonalInfo}
+                    name="congenital"
+                  />
+                  <h5 className="inline" style={{ marginLeft: '30px' }}>ยาที่แพ้</h5>
+                  <input 
+                    type="text" 
+                    className="input-200" 
+                    value={data.personalInfo.allergyDrug}
+                    onChange={this.handlePersonalInfo}
+                    name="allergyDrug"
+                  />
                 </div>
-                <div style={{marginTop: '20px'}}>
-                  <h5 className="inline">โทรศัพท์</h5>
-                  <input type="text" className="input-200" value={this.show("tel","patientInformation")}/>
-                  <h5 className="inline" style={{ marginLeft: '30px' }}>อีเมล</h5>
-                  <input type="text" className="input-200" value={this.show("email","patientInformation")}/>
+                <div style={{ marginTop: '20px' }}>
+                  <h5 className="inline" style={{ marginLeft: '30px' }}>ผู้ติดต่อฉุกเฉิน</h5>
+                  <input 
+                    type="text" 
+                    className="input-200" 
+                    value={data.personalInfo.contact}
+                    onChange={this.handlePersonalInfo}
+                    name="contact"
+                  />
+                </div>
+              </div>
+              <div style={{ width: '100%', padding: '20px' }}>
+                <h4>ข้อมูลติดต่อของผู้ป่วย</h4>
+                <hr />
+                <div style={{ marginLeft: '30px' }}>
+                  <div>
+                    <h5>ที่อยู่ปัจจุบัน</h5>
+                    <textarea 
+                      cols="80" 
+                      rows="4" 
+                      onChange={this.handlePatientInformation} 
+                      name="address"
+                      value={data.patientInformation.address}
+                    >
+                      {data.patientInformation.address}
+                    </textarea>
+                  </div>
+                  <div style={{marginTop: '20px'}}>
+                    <h5 className="inline">โทรศัพท์</h5>
+                    <input 
+                      type="text" 
+                      className="input-200" 
+                      value={data.patientInformation.tel}
+                      onChange={this.handlePatientInformation} 
+                      name="tel"
+                    />
+                    <h5 className="inline" style={{ marginLeft: '30px' }}>อีเมล</h5>
+                    <input 
+                      type="text" 
+                      className="input-200" 
+                      value={data.patientInformation.email}
+                      onChange={this.handlePatientInformation} 
+                      name="email"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
