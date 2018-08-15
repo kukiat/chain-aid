ad6843e269e76365bd08ee28ac8dfe32f945677a
blaster666 <aiy.spell@gmail.com>
2018-08-16 02:06:04 +0700

    fetch form stupid

--- src/components/dashboard/Manage.js
+++ src/components/dashboard/Manage.js
@@ -7,7 +7,27 @@ class Manage extends React.Component {
     super();
     this.state = {
       lastModifile: '2018-11-8',
+      data: []
     };
+  }
+  componentDidMount(){
+    fetch('https://chain-aid-api.herokuapp.com/api/manage/?cardid=5704062616518').
+    then(response => response.json()).
+    then(result => {
+      this.setState({ data: result.results[0]})})
+  }
+  show(value, value2){
+    if (this.state.data[value2] != undefined){
+      return  this.state.data[value2][value]
+    }
+    return ""
+  }
+  age(birthday){
+      let textArr = birthday.split('-')
+      let dob = new Date(textArr[2]-543, textArr[1], textArr[0])
+      let diff_ms = Date.now() - dob.getTime();
+      let age_dt = new Date(diff_ms); 
+      return Math.abs(age_dt.getUTCFullYear() - 1970);
   }
   render() {
     return (
@@ -26,15 +46,15 @@ class Manage extends React.Component {
             <div style={{ padding: '10px 30px' }} className="inline">
               <div style={{ marginTop: '10px' }}>
                 <h5 className="inline">หมายเลขประจำตัวผู้ป่วย</h5>
-                <input type="text" className="input-200" />
+                <input type="text" className="input-200" value={this.show("patientId","manageId")}/>
               </div>
               <div style={{ marginTop: '10px' }}>
                 <h5 className="inline">หมายเลขประจำตัวประชาชน</h5>
-                <input type="text" className="input-200" />
+                <input type="text" className="input-200" value={this.show("cardId","manageId")}/>
               </div>
               <div style={{ marginTop: '10px' }}>
                 <h5 className="inline">หมายเลขประจำตัวต่างด้าว</h5>
-                <input type="text" className="input-200" />
+                <input type="text" className="input-200" value={this.show("alienId","manageId")}/>
               </div>
             </div>
             <div className="inline" style={{marginLeft: '20vw'}}>
@@ -47,69 +67,69 @@ class Manage extends React.Component {
                 <div className="row">
                   <div className="col">
                     <h5 className="inline">ชื่อ</h5>
-                    <input type="text" className="input-150" />
+                    <input type="text" className="input-150" value={this.show("name","personalInfo")}/>
                   </div>
                   <div className="col">
                     <h5 className="inline" style={{marginLeft: '30px' }}>นามสกุล</h5>
-                    <input type="text" className="input-150" />
+                    <input type="text" className="input-150" value={this.show("surname","personalInfo")}/>
                   </div>
                   <div className="col">
                     <h5 className="inline" style={{ marginLeft: '30px' }}>ชื่อเล่น</h5>
-                    <input type="text" className="input-100" />
+                    <input type="text" className="input-100" value={this.show("nickname","personalInfo")}/>
                   </div>
                 </div>
                 <div style={{marginTop: '20px'}} className="row">
                   <div className="col">
                     <h5 className="inline">เพศ</h5>
-                    <input type="text" className="input-150" />
+                    <input type="text" className="input-150" value={this.show("gender","personalInfo")}/>
                   </div>
                   <div className="col">
                     <h5 className="inline" style={{ marginLeft: '30px' }}>วันเกิด</h5>
-                    <input type="text" className="input-150" />
+                    <input type="text" className="input-150" value={this.show("birthday","personalInfo")}/>
                   </div>
                   <div className="col">
                     <h5 className="inline" style={{ marginLeft: '30px' }}>อายุ</h5>
-                    <input type="text" className="input-50"/>
+                    <input type="text" className="input-50" value={this.age(this.show("birthday","personalInfo"))}/>
                   </div>
                 </div>
                 <div style={{ marginTop: '20px' }} className="row">
                 <div className="col">
                   <h5 className="inline">ส่วนสูง</h5>
-                  <input type="text" className="input-50" />
+                  <input type="text" className="input-50" value={this.show("height","personalInfo")}/>
                 </div>
                 <div className="col">
                   <h5 className="inline" style={{ marginLeft: '30px' }}>น้ำหนัก</h5>
-                  <input type="text" className="input-50" />
+                  <input type="text" className="input-50" value={this.show("weight","personalInfo")}/>
                 </div>
                 <div className="col">
                   <h5 className="inline" style={{ marginLeft: '30px' }}>หมู่เลือด</h5>
-                  <input type="text" className="input-50" />
+                  <input type="text" className="input-50" value={this.show("bloodType","personalInfo")}/>
                 </div>
                 </div>
               </div>
               <div style={{ marginTop: '20px' }} className="row">
               <div className="col">
                 <h5 className="inline" style={{ marginLeft: '30px' }}>สัญชาติ</h5>
-                <input type="text" className="input-100" />
+                <input type="text" className="input-100" value={this.show("nation","personalInfo")}/>
               </div>
               <div className="col">
                 <h5 className="inline" style={{ marginLeft: '30px' }}>เชื้อชาติ</h5>
-                <input type="text" className="input-100" />
+                <input type="text" className="input-100" value={this.show("race","personalInfo")}/>
               </div>
               <div className="col">
                 <h5 className="inline" style={{ marginLeft: '30px' }}>ศาสนา</h5>
-                <input type="text" className="input-100" />
+                <input type="text" className="input-100" value={this.show("religion","personalInfo")}/>
               </div>
               </div>
               <div style={{ marginTop: '20px' }}>
                 <h5 className="inline" style={{ marginLeft: '30px' }}>โรคประจำตัว</h5>
-                <input type="text" className="input-200" />
+                <input type="text" className="input-200" value={this.show("congenital","personalInfo")}/>
                 <h5 className="inline" style={{ marginLeft: '30px' }}>ยาที่แพ้</h5>
-                <input type="text" className="input-200" />
+                <input type="text" className="input-200" value={this.show("allergyDrug","personalInfo")}/>
               </div>
               <div style={{ marginTop: '20px' }}>
                 <h5 className="inline" style={{ marginLeft: '30px' }}>ผู้ติดต่อฉุกเฉิน</h5>
-                <input type="text" className="input-200" />
+                <input type="text" className="input-200" value={this.show("contact","personalInfo")}/>
               </div>
             </div>
             <div style={{ width: '100%', padding: '20px' }}>
@@ -118,13 +138,13 @@ class Manage extends React.Component {
               <div style={{ marginLeft: '30px' }}>
                 <div>
                   <h5>ที่อยู่ปัจจุบัน</h5>
-                  <textarea cols="80" rows="4"></textarea>
+                  <textarea cols="80" rows="4">{this.show("address","patientInformation")}</textarea>
                 </div>
                 <div style={{marginTop: '20px'}}>
                   <h5 className="inline">โทรศัพท์</h5>
-                  <input type="text" className="input-200" />
+                  <input type="text" className="input-200" value={this.show("tel","patientInformation")}/>
                   <h5 className="inline" style={{ marginLeft: '30px' }}>อีเมล</h5>
-                  <input type="text" className="input-200" />
+                  <input type="text" className="input-200" value={this.show("email","patientInformation")}/>
                 </div>
               </div>
             </div>
